f7b02e49a78a36d5914ed2195f2a0c1e6b0a5735
Kadlecik <ondrej.kadlecik@seznam.cz>
2024-11-13 18:52:54 +0100

    vytvoření a spuštění programu úkol 1

--- ukoly.py
+++ ukoly.py
@@ -3,16 +3,14 @@ f.write("Hello world file")
 f.close()
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+    print(f'Hi, {name}')  
 
 f2 = open("test1.txt", "r")
 data = f2.read()
 f2.close()
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print(data)
